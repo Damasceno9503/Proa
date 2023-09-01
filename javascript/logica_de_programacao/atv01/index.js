d5daf40eb2d1c91983fce7596eda804fa9d94466
Felipe Santos Damasceno <126077568+Damasceno9503@users.noreply.github.com>
2023-08-31 22:19:38 -0300

    Add files via upload

---
+++ javascript/logica_de_programacao/atv01/index.js
@@ -0,0 +1,42 @@
+//Escreva um programa em Javascript em que o usuário informe o seu nome e em seguida o programa perguntará a idade do usuário. Agora o programa deve exibir a mensagem "Olá, [NomeDoUsuario], sua idade é [idade]".
+var nome = prompt("Qual é seu nome? ");
+var idade = prompt("Qual é seu idade? ");
+
+document.write("Olá, " + nome + " " + idade);
+
+//Considerando a figura abaixo, escreva um programa para cada forma que calcule e exiba em tela cada uma de suas respectivas áreas. O usuário irá informar os valores de cada variável.
+
+var rtg_bs = prompt("Para calcular o tamanho de um retângulo, diga qual é a base: ");
+var rtg_al = prompt("Agora diga a altura: ");
+var rtg_res = rtg_bs * rtg_al;
+document.write("O resultado é " + rtg_res);
+
+var qdo = prompt("Para calcular o tamanho de um quadrado, diga qual é a base: ");
+var qdo_res = qdo * qdo;
+document.write("O resultado é " + qdo_res);
+
+var lsg_mai = prompt("informe o valor da maior diagonal: ");
+var lsg_men = prompt("Informe o valor da menor diagonal: ");
+var lsg_res = (lsg_mai * lsg_men) / 2;
+document.write("O resultado é " + lsg_res);
+
+var tpz_mai = prompt("Qual e a maior parte do trapézio: ");
+var tpz_men = prompt("Qual e a menor parte do trapézio: ");
+var alt = prompt("Qual a altura do trapézio: ");
+var tpz_res = (parseFloat(tpz_mai) + parseFloat(tpz_men)) * parseFloat(alt) /2;
+document.write("O resultado e " + tpz_res);
+
+var paralelogramo_bs = prompt("Informe a base do paralelogramo: ");
+var paralelogramo_alt = prompt("Informe a altura: ");
+var paralelogramo_res = parseFloat(paralelogramo_bs) * parseFloat(paralelogramo_alt);
+document.write("O resultado é " + paralelogramo_res);
+
+var triangulo_bs = prompt("Qual é a base do triangulo? ")
+var triangulo_alt = prompt("Qual é a altura do triangulo? ")
+var triangulo_res = parseFloat(triangulo_bs) * parseFloat(triangulo_alt) / 2;
+document.write("O resultado é " + triangulo_res);
+
+var circulo = prompt("Qual é o raio do círculo? ");
+var circulo_res = 3.141 * parseFloat(circulo) * 2;
+document.write("O resultado é " + circulo_res);
+
